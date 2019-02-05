9e8d0cb5de6a8a3a8b18ad0ed0bc7a269e316776
Jani Ristinen <jani@Janis-MacBook-Pro.local>
2019-02-05 21:06:57 +0200

    Commit

---
+++ src/main/java/hh/swd20/Bookstore/web/BookController.java
@@ -0,0 +1,19 @@
+package hh.swd20.Bookstore.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class BookController
+{
+
+	@RequestMapping(value="/index", method = RequestMethod.GET)
+	public String index()
+	{
+		return "index";
+	}
+	
+}
