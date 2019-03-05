1ed7aab712af2df771f6a73eb38b43e36b5a008e
Jani Ristinen <jani@Janis-MacBook-Pro.local>
2019-03-05 21:44:25 +0200

    REST toimintoja

--- src/main/java/hh/swd20/Bookstore/web/BookController.java
+++ src/main/java/hh/swd20/Bookstore/web/BookController.java
@@ -1,6 +1,7 @@
 package hh.swd20.Bookstore.web;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import hh.swd20.Bookstore.domain.Book;
 import hh.swd20.Bookstore.domain.BookRepository;
@@ -33,6 +34,18 @@ public class BookController
 		
 		return "booklist";
 	}
+	
+	
+    @RequestMapping(value="/books", method = RequestMethod.GET)
+    public @ResponseBody List<Book> booktListRest() {	
+        return (List<Book>) repository.findAll();
+    }    
+
+
+    @RequestMapping(value="/book/{id}", method = RequestMethod.GET)
+    public @ResponseBody Optional<Book> findBookRest(@PathVariable("id") Long id) {	
+    	return repository.findById(id);
+    }       
 	
 	@RequestMapping(value="/newbook", method = RequestMethod.GET)
 	public String getNewBookForm(Model model)
